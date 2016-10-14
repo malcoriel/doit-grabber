b4f606d5c1445c7b4c5b446d188a344f34e61be8
Valeriy Kuzmin <vkuzmin@kontur.ru>
2016-10-14 21:58:08 +0500

    writing to file, auth from argv

--- src/grabber.js
+++ src/grabber.js
@@ -6,25 +6,29 @@ prompt.message = '';
 prompt.delimiter = '';
 import Q from 'q';
 import {argv} from 'yargs';
-
+import fs from 'fs';
 function main() {
-	const loginSchema = {
-		properties: {
-			login: {
-				message: 'doit.im login:',
-				required: true,
-			},
-			password: {
-				message: 'password:',
-				hidden: true,
-				required: true,
-			},
-		},
-	};
 	return Q()
-		.then(() => Q.fcall(prompt.start))
-		.then(function askForCredentials() {
-			return Q.nbind(prompt.get, prompt)(loginSchema);
+		.then(() => {
+			if (argv.login && argv.password)
+				return { login: argv.login, password: argv.password };
+			return Q.fcall(prompt.start)
+				.then(function askForCredentials() {
+					const loginSchema = {
+						properties: {
+							login: {
+								message: 'doit.im login:',
+								required: true,
+							},
+							password: {
+								message: 'password:',
+								hidden: true,
+								required: true,
+							},
+						},
+					};
+					return Q.nbind(prompt.get, prompt)(loginSchema);
+				});
 		})
 		.then(function tryAuthenticate(promptResult) {
 			return lib.auth(promptResult);
@@ -32,7 +36,13 @@ function main() {
 		.then(() => {
 			return lib.getAllTasks();
 		})
-		.tap(console.log)
+		.then((tasks) => {
+			if(argv.output)
+			{
+				return Q.fcall(fs.writeFile, argv.output, JSON.stringify(tasks, null, 4));
+			}
+			console.log(tasks);
+		})
 		.then(() => console.log('done'));
 }
 main();
