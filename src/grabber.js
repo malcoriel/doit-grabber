baaba5a10932d0f71be6ca54c55cb48a7536653c
Valeriy Kuzmin <vkuzmin@kontur.ru>
2016-10-14 13:01:01 +0500

    skeleton

--- src/grabber.js
+++ src/grabber.js
@@ -1,3 +1,40 @@
 import DoItLib from './lib/lib';
 const lib = new DoItLib();
-lib.method();
+import prompt from 'prompt';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+prompt.message = '';
+prompt.delimiter = '';
+import Q from 'q';
+import {argv} from 'yargs';
+
+function main() {
+	const loginSchema = {
+		properties: {
+			login: {
+				message: 'doit.im login:',
+				required: true,
+			},
+			password: {
+				message: 'password:',
+				hidden: true,
+				required: true,
+			},
+		},
+	};
+	return Q()
+		.then(() => Q.fcall(prompt.start))
+		.then(function askForCredentials() {
+			return Q.nbind(prompt.get, prompt)(loginSchema);
+		})
+		.then(function tryAuthenticate(promptResult) {
+			return lib.auth(promptResult);
+		})
+		.then(() => {
+			return lib.getTaskList();
+		})
+		.tap(console.log);
+}
+main();
+
+
+
--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,5 +1,17 @@
+const agent = require('superagent-promise')(require('superagent'), Q.Promise);
+import Q from 'q';
+
 export default class DoItLib {
 	method(){
 		console.log('foo');
+	}
+
+	auth({login,password}){
+		console.log(`mock auth for ${login} success`);
+		return Q();
+	}
+
+	getTaskList(){
+		return Q(['mock-task-1','mock-task-2']);
 	}
 }
